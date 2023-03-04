c3bdf21b62671b9bed7390ed6cd469019440e53e
Charles O'Brien <charlesobriennb@gmail.com>
2023-03-04 15:45:47 -0400

    Add project files.

---
+++ TinyAPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+WebApplicationBuilder webApplicationBuilder =
+    WebApplication.CreateBuilder(args);
+
+WebApplication webApplication =
+    webApplicationBuilder.Build();
+
+
+
+webApplication.MapGet("/api/home",
+    (Func<string>)(() => "This is the home endpoint"));
+
+webApplication.Run();
